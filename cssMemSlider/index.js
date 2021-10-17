3673ccbf729f8ad21a78721d7e85fe6b206ce376
kykysja <kykysja11@gmail.com>
2021-10-17 19:56:00 +0200

    feat: add animation (Sun, Oct 17, 2021 17:55 PM)

--- cssMemSlider/index.js
+++ cssMemSlider/index.js
@@ -2,6 +2,7 @@ const slides = document.querySelectorAll(".slide-item");
 const slidesContainer = document.querySelector(".slider-inner-container");
 const bulletsContainer = document.querySelector(".slider-bullets");
 const bullets = document.querySelectorAll(".bullet-wrap");
+const slideDescriptions = document.querySelectorAll(".slide-description");
 
 let activeSlideNumber = 0;
 
@@ -16,9 +17,14 @@ bulletsContainer.addEventListener("click", (event) => {
   if (event.target.classList.contains("bullet-wrap")) {
     console.log(event.target);
     activeSlideNumber = event.target.id[0];
+    slideDescriptions.forEach((el) => (el.style.opacity = "0"));
   }
   moveSlider();
 });
+
+window.addEventListener("transitionend", () =>
+  slideDescriptions.forEach((el) => (el.style.opacity = "100%")),
+);
 
 window.addEventListener("resize", () => {
   moveSlider();
