ab99261387ee7962eddfa072d373bb5fa05aa6a1
kykysja <kykysja11@gmail.com>
2021-10-17 20:25:31 +0200

    fix: fix animation's bugs (Sun, Oct 17, 2021 18:25 PM)

--- cssMemSlider/index.js
+++ cssMemSlider/index.js
@@ -9,21 +9,22 @@ let activeSlideNumber = 0;
 function moveSlider() {
   const slideWidth = slides[0].offsetWidth;
   const offset = -1 * (activeSlideNumber - 1) * slideWidth;
-  console.log(offset);
+
   slidesContainer.style.transform = `translateX(${offset}px)`;
 }
 
 bulletsContainer.addEventListener("click", (event) => {
   if (event.target.classList.contains("bullet-wrap")) {
-    console.log(event.target);
+    if (activeSlideNumber !== event.target.id[0]) {
+      slideDescriptions.forEach((el) => (el.style.color = "darkslategray"));
+    }
     activeSlideNumber = event.target.id[0];
-    slideDescriptions.forEach((el) => (el.style.opacity = "0"));
   }
   moveSlider();
 });
 
 window.addEventListener("transitionend", () =>
-  slideDescriptions.forEach((el) => (el.style.opacity = "100%")),
+  slideDescriptions.forEach((el) => (el.style.color = "bisque")),
 );
 
 window.addEventListener("resize", () => {
